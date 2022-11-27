6d2d61aff2a4027053149828506200d72fc28957
smily000 <91569111+smily000@users.noreply.github.com>
2022-11-27 16:52:52 -0600

    lol

--- client/map.js
+++
@@ -1,8 +0,0 @@
-var gMap; 
-
-function initmap(){
-    gMap = new google.maps.Map(document.getElementById('mapid'),{center:{lat:41.878,lng:10},zoom:3});
-}
-function intiApplication(){
-    console.log('map mania')
-}
